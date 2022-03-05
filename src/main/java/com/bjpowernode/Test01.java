964803d96c6a7acf36eb2a2cda13e2845b840efc
zs <1598872182@qq.com>
2022-03-05 21:12:46 +0800

    2

---
+++ src/main/java/com/bjpowernode/Test01.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+        System.out.println(i);
+
+
+    }
+}
